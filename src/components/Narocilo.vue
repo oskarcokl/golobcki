530bc0065d1992586dc3e44cdc86494f57534046
gabers <eg0458@student.uni-lj.si>
2021-05-22 23:06:02 +0200

    reeee it works now mostly reeee

--- src/components/Narocilo.vue
+++ src/components/Narocilo.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="narocilo row">
-    <div class="card-image-container col-sm-6">
-      <img class="card-image" src="../../public/avatar.png" alt="" />
+    <div class="card-image-container col-sm-6 ">
+      <div class="ree">
+      <img class="card-image float-lg-right img-fluid" src="../../public/avatar.png" alt="" />
+      </div>
     </div>
     <div class="data col-sm-6">
-      <p class="name">Ime paketa</p>
-      <input v-model="message" placeholder="Knijga">
-      <p class="velikost">Velikost paketa</p>
-      <select v-model="selected">
-        <option disabled value="">Izberite velikost</option>
-        <option>Majhen</option>
-        <option>Velik ( ͡° ͜ʖ ͡°)</option>
-      </select>
-      <p class="prevzem">Lokacija prevzema</p>
-      <input v-model="message" placeholder="Ljubljana, Svetčeva 9">
-      <p class="oddaja">Lokacija oddaje</p>
-      <input v-model="message" placeholder="Ljubljana, Tržaška 7">
-      <p class="cena">Plačilo</p>
-      <input type="number" v-model="message" placeholder="5€">
-      <br>
-      <br>
-      <button class="gumb">Oddaj dostavo</button>
+      <div class="float-lg-left text-sm-left">
+        <p class="name">Ime paketa</p>
+        <input v-model="message" placeholder="Knijga">
+        <p class="velikost">Velikost paketa</p>
+        <select v-model="selected">
+          <option disabled value="">Izberite velikost</option>
+          <option>Majhen</option>
+          <option>Velik ( ͡° ͜ʖ ͡°)</option>
+        </select>
+        <p class="prevzem">Lokacija prevzema</p>
+        <input v-model="message" placeholder="Ljubljana, Svetčeva 9">
+        <p class="oddaja">Lokacija oddaje</p>
+        <input v-model="message" placeholder="Ljubljana, Tržaška 7">
+        <p class="cena">Plačilo</p>
+        <input type="number" v-model="message" placeholder="5€">
+        <br>
+        <br>
+        <button class="gumb">Oddaj dostavo</button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,15 +40,11 @@ export default {
   color: #6f6f6f;
 }
 .card-image {
-  width: 300px;
-  height: auto;
   border-style: solid;
   border-width: 5px;
   border-color: #6f6f6f;
 }
-.card-image-container{
-  text-align: right;
-}
+
 .gumb{
   color: #ffffff;
   background-color: #e06D6D;
@@ -53,8 +53,8 @@ export default {
   border-width: 5px;
   border-color: #c30909;
 }
-.data{
-  text-align: left;
+.ree{
+  max-width: 300px;
 }
 input, select{
   background-color: #c4c4c4;
