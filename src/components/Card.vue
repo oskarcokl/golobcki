967ee18a7c75f521a0e35768a7ae92c91bef823d
oskarcokl <oskar.cokl@gmail.com>
2021-05-22 16:21:23 +0200

    feat: Started working on card component.

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-container">
+    <div class="card-image-container">
+      <img class="card-image" src="../../public/avatar.png" alt="" />
+    </div>
+    <div class="card-content">
+      <h2 class="card-title">Vrnitev ključev</h2>
+      <ul class="order-information-list">
+        <li class="order-information-item">Manjši paket</li>
+        <li class="order-information-item">Ljubljana, Vodnikova 6</li>
+        <li class="order-information-item">Ljubljana, Ulica Ivane Kobilce 4</li>
+        <li class="order-information-item">5€</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style scoped>
+.card-container {
+  background-color: #e3e3e3;
+  text-align: left;
+  display: inline-block;
+  width: 40%;
+  padding: 10px, 5px;
+}
+
+.card-image {
+  width: 150px;
+  height: auto;
+}
+</style>
--- src/views/FrontPageView.vue
+++ src/views/FrontPageView.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Hello welcome to Golobcki</h1>
+  <div class="container">
+    <h1>Hello welcome to Golobcki</h1>
+    <card />
+  </div>
 </template>
 
 <script>
+import Card from "../components/Card";
 export default {
   name: "FrontPageView",
+  components: {
+    Card,
+  },
 };
 </script>
