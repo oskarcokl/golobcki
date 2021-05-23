88739a7e7fb2a2090dbb296c61c17bca69fff00a
oskarcokl <oskar.cokl@gmail.com>
2021-05-23 10:57:36 +0200

    v 0.1.2 forgot to build lol.

--- dist/js/chunk-2d216eae.10130ead.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d216eae"],{c506:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("Example 2")])},l=[],u=t("2877"),a={},r=Object(u["a"])(a,c,l,!1,null,null,null);n["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d216eae.10130ead.js.map
